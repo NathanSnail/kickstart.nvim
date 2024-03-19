33f8874c13c17cfd6e77dd63d3df8ee039c2d970
NathanSnail <nathan.titirangi@gmail.com>
2024-03-19 17:07:24 +1300

    Fully files

---
+++ lua/custom/plugins/NeogitOrg_neogit.lua
@@ -0,0 +1,9 @@
+return 	{dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+
+			"nvim-telescope/telescope.nvim",
+		},
+		config = true,
+	}
+
---
+++ lua/custom/plugins/Saecki_crates_nvim.lua
@@ -0,0 +1,8 @@
+return 	{event = { "BufRead Cargo.toml" },
+		opts = {
+			src = {
+				cmp = { enabled = true },
+			},
+		},
+	}
+
