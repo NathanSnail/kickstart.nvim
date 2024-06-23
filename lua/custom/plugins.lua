a8c6297ebde4a4aeb394d1101c69d45d62c2cf9c
Nathan <nathan.titirangi@gmail.com>
2024-06-23 21:04:30 +1200

    doesnt work

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -70,11 +70,4 @@ return {
 	},
 	"lambdalisue/suda.vim",
 	"mbbill/undotree",
-	{
-		"vhyrro/luarocks.nvim",
-		priority = 1001, -- this plugin needs to run before anything else
-		opts = {
-			rocks = { "magick" },
-		},
-	},
 }
