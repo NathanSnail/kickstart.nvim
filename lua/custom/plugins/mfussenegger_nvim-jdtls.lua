f1ab884efb3c0943d3c8df212392becddf216179
Nathan <47441022+NathanSnail@users.noreply.github.com>
2025-10-31 20:52:02 +1300

    jdtls multiple file project correct buffer usage

--- lua/custom/plugins/mfussenegger_nvim-jdtls.lua
+++ lua/custom/plugins/mfussenegger_nvim-jdtls.lua
@@ -3,25 +3,30 @@ return {
 	dependencies = { "nvim-dap" },
 	config = function()
 		local jdtls = require("jdtls")
-		local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
-		local root_dir = require("jdtls.setup").find_root(root_markers)
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "java",
+			callback = function()
+				local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
+				local root_dir = require("jdtls.setup").find_root(root_markers)
 
-		if root_dir then
-			local config = {
-				cmd = {
-					"jdtls",
-				},
-				root_dir = root_dir,
-				settings = {
-					java = {
-						-- Java-specific settings
+				if not root_dir then return end
+
+				local config = {
+					cmd = {
+						"jdtls",
 					},
-				},
-				init_options = {
-					bundles = {},
-				},
-			}
-			jdtls.start_or_attach(config)
-		end
+					root_dir = root_dir,
+					settings = {
+						java = {
+							-- Java-specific settings
+						},
+					},
+					init_options = {
+						bundles = {},
+					},
+				}
+				jdtls.start_or_attach(config)
+			end,
+		})
 	end,
 }
